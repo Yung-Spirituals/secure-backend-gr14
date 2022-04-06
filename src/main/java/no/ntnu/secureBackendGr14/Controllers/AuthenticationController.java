cdb6a8046b2697a411716ad6b77ac8516ab9b875
Aron Selven <aronselven@gmail.com>
2022-04-06 23:39:15 +0200

    Added authentication controller

---
+++ src/main/java/no/ntnu/secureBackendGr14/Controllers/AuthenticationController.java
@@ -0,0 +1,45 @@
+package no.ntnu.secureBackendGr14.Controllers;
+
+import no.ntnu.secureBackendGr14.security.AuthenticationRequest;
+import no.ntnu.secureBackendGr14.security.AuthenticationResponse;
+import no.ntnu.secureBackendGr14.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController
+{
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest)
+    {
+        try
+        {
+            this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUsername(),
+                    authenticationRequest.getPassword()));
+            final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+            final String jwtUtil = this.jwtUtil.generateToken(userDetails);
+            return ResponseEntity.ok(new AuthenticationResponse(jwtUtil));
+        }
+        catch (BadCredentialsException e)
+        {
+            return new ResponseEntity<>("Invalid credentials", HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
