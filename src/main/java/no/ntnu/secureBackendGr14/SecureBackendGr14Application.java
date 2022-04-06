1a2290dfb3f1383c915245fec95c64e98b85ff07
Aron Selven <aronselven@gmail.com>
2022-04-06 11:17:34 +0200

    initial commit

---
+++ src/main/java/no/ntnu/secureBackendGr14/SecureBackendGr14Application.java
@@ -0,0 +1,13 @@
+package no.ntnu.secureBackendGr14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBackendGr14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBackendGr14Application.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/secureBackendGr14/SecureBackendGr14ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.secureBackendGr14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBackendGr14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
