3a586fb7e0dcb30353c0d41cb412c62197323c56
Aron Selven <aronselven@gmail.com>
2022-04-06 23:51:28 +0200

    Added a test controller

---
+++ src/main/java/no/ntnu/secureBackendGr14/Controllers/TestController.java
@@ -0,0 +1,31 @@
+package no.ntnu.secureBackendGr14.Controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class TestController
+{
+    @GetMapping("")
+    public String testPublic()
+    {
+        return "This is public :)";
+    }
+
+    @GetMapping("user")
+    @PreAuthorize("hasRole('USER')")
+    public String testUser()
+    {
+        return "This is user level :D";
+    }
+
+    @GetMapping("admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String testAdmin()
+    {
+        return "This is admin level!!! ;D";
+    }
+}
